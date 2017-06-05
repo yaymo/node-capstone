dbaec373f7cb00f6eac8000d1da17141af16be66
yaymo <stevenjamesonhill@gmail.com>
2017-06-05 11:21:44 -0400

    fixed and implemented basic authentication for login

--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
-
-
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        'mongodb://jamesonhill:123456@ds149491.mlab.com:49491/my-shows-app';
 
 exports.PORT = process.env.PORT || 8080;
+
