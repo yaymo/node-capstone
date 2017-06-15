94854ef65641f0f1e011ba0096cc1da7589e03d6
yaymo <stevenjamesonhill@gmail.com>
2017-06-14 22:02:11 -0400

    get and post tests working

--- test/test-integrations.js
+++ test/test-integrations.js
@@ -2,18 +2,159 @@ const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
+const mongoose = require('mongoose');
+const faker = require('faker');
+const {User} = require('../app/models/user');
 
 
-const app = server.app;
+const {app, runServer, closeServer} = require('../server');
+const {Show} = require('../app/models/show');
+const {TEST_DATABASE_URL} = require('../config/database');
 chai.use(chaiHttp);
 
+
+function seedShowData() {
+  const seedData = [];
+
+  for(let i=0; i<=10; i++) {
+    seedData.push(generateShowData());
+  }
+
+  return Show.insertMany(seedData);
+}
+
+function generateShowData() {
+  return {
+    title: faker.lorem.word()
+  }
+}
+
+function generateUserLogin() {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password()
+  }
+}
+
+function seedUserLogin() {
+  const seedData = [];
+
+  for(let i=0; i<=10; i++) {
+    seedData.push(generateUserLogin());
+  }
+  return User.insertMany(seedData);
+}
+
+function tearDownDb() {
+  return mongoose.connection.dropDatabase();
+}
+
+
+
 describe('initial get request tests', () => {
-  it('should return initial request status', () => {
-    return chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.should.be.html;
-      });
+    it('should render login page', () => {
+      return chai.request(app)
+        .get('/')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.html;
+        });
+    });
+});
+
+describe('shows app integration testing', () => {
+  before(() => {
+    return runServer(TEST_DATABASE_URL);
   });
+
+  beforeEach(() => {
+    return seedUserLogin();
+  });
+
+  afterEach(() => {
+    return tearDownDb();
+  });
+
+  after(() => {
+    return closeServer();
+  });
+
+  describe('get endpoint', () => {
+    it('should return 200', () => {
+      let res;
+      return chai.request(app)
+        .get('/')
+        .then((_res) => {
+          res = _res;
+          res.should.have.status(200);
+        })
+    });
+
+  describe('post endpoint', () => {
+    it('should add a new user', () => {
+      const newUser = generateUserLogin();
+
+      return chai.request(app)
+        .post('/users')
+        .send(newUser)
+        .then((res) => {
+          res.should.be.json;
+          res.should.have.status(201);
+          res.body.should.be.a('object');
+          res.body.should.include.keys('username', 'id');
+          res.body.id.should.not.be.null;
+        });
+    });
+  });
+
+
+  describe('user put endpoint', () => {
+    it('should update a post', () => {
+      const updatedUser = generateUserLogin();
+
+      return User
+        .findOne()
+        .exec()
+        .then((user) => {
+          updatedUser.id = user._id;
+
+          return chai.request(app)
+            .put(`/users/${user._id}`)
+            .send(updatedUser);
+        })
+
+        .then(res => {
+          res.should.have.status(204);
+          return User.findById(updatedUser.id).exec();
+        })
+
+        .then((user) => {
+          user.username.should.equal(updatedUser.username);
+        });
+    });
+  });
+
+  describe('delete endpoint', () => {
+    it('should remove user from db', () => {
+      let user;
+
+      return User
+        .findById()
+        .exec()
+        .then((_user) => {
+          return chai.request(app).delete(`/users/${user.id}`);
+        })
+
+        .then((res) => {
+          res.should.have.status(204)
+
+          return User.findById(user.id);
+        })
+
+        .then(_user => {
+          should.not.exist(_user);
+        });
+    });
+  });
+});
 });
