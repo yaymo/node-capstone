0f148589d7fd75eb01ac7c890eb8472bc1774bec
yaymo <stevenjamesonhill@gmail.com>
2017-06-30 12:13:10 -0400

    updated config db, home page markup, and removed console.log of user data

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        'mongodb://jamesonhill:123456@ds149491.mlab.com:49491/my-shows-app';
 
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
-                        'mongodb://jamesonhill:123456@ds149491.mlab.com:49491/test-my-shows-app';
+exports.TEST_DATABASE_URL = ( process.env.TEST_DATABASE_URL ||
+                        'mongodb://jamesonhill:12345@ds149491.mlab.com:49491/test-my-shows-app');
 
 exports.PORT = process.env.PORT || 8080;
 
