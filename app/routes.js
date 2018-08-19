92b7a48d08cacb536c6e05871a7849c50ab70c90
yaymo <stevenjamesonhill@gmail.com>
2018-08-19 18:54:33 -0400

    update routes and front end

--- app/routes.js
+++ app/routes.js
@@ -1,162 +1,167 @@
-const {BasicStrategy} = require('passport-http');
-const passport = require('passport');
+const { BasicStrategy } = require("passport-http");
+const passport = require("passport");
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {User} = require('./models/user');
+const { User } = require("./models/user");
 
 router.use(jsonParser);
 
+console.log("test");
 
-
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   if (!req.body) {
-    return res.status(400).json({message: 'No request body'});
+    return res.status(400).json({ message: "No request body" });
   }
-  if (!('username' in req.body)) {
-    return res.status(422).json({message: 'Missing field: username'});
+  if (!("username" in req.body)) {
+    return res.status(422).json({ message: "Missing field: username" });
   }
 
-  let {username, password, firstName, lastName} = req.body;
+  let { username, password, firstName, lastName } = req.body;
 
-  if (typeof username !== 'string') {
-    return res.status(422).json({message: 'Incorrect field type: username'});
+  if (typeof username !== "string") {
+    return res.status(422).json({ message: "Incorrect field type: username" });
   }
 
   username = username.trim();
 
-  if (username === '') {
-    return res.status(422).json({message: 'Incorrect field length: username'});
+  if (username === "") {
+    return res
+      .status(422)
+      .json({ message: "Incorrect field length: username" });
   }
 
-  if (!(password)) {
-    return res.status(422).json({message: 'Missing field: password'});
+  if (!password) {
+    return res.status(422).json({ message: "Missing field: password" });
   }
 
-  if (typeof password !== 'string') {
-    return res.status(422).json({message: 'Incorrect field type: password'});
+  if (typeof password !== "string") {
+    return res.status(422).json({ message: "Incorrect field type: password" });
   }
 
   password = password.trim();
 
-  if (password === '') {
-    return res.status(422).json({message: 'Incorrect field length: password'});
+  if (password === "") {
+    return res
+      .status(422)
+      .json({ message: "Incorrect field length: password" });
   }
-  return User
-    .find({username})
+  return User.find({ username })
     .count()
     .exec()
     .then(count => {
       if (count > 0) {
         return Promise.reject({
-          name: 'authenticationerror',
-          message: 'username already taken'
+          name: "authenticationerror",
+          message: "username already taken"
         });
       }
-      return User.hashPassword(password)
+      return User.hashPassword(password);
     })
     .then(hash => {
-      return User
-        .create({
-          username: username,
-          password: hash,
-          firstName: firstName,
-          lastName: lastName
-        })
+      return User.create({
+        username: username,
+        password: hash,
+        firstName: firstName,
+        lastName: lastName
+      });
     })
     .then(user => {
       return res.status(201).json(user.apiRepr());
     })
     .catch(err => {
-      if(err.name === 'authenticationerror'){
-        return res.status(422).json({message: err.message});
+      if (err.name === "authenticationerror") {
+        return res.status(422).json({ message: err.message });
       }
-      res.status(500).json({message:'Internal Server Error'})
+      res.status(500).json({ message: "Internal Server Error" });
     });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   if (!req.body) {
-    return res.status(400).json({message: 'No request body'});
+    return res.status(400).json({ message: "No request body" });
   }
-  if (!('username' in req.body)) {
-    return res.status(422).json({message: 'Missing field: username'});
+  if (!("username" in req.body)) {
+    return res.status(422).json({ message: "Missing field: username" });
   }
 
-  let {username, password, firstName, lastName} = req.body;
+  let { username, password, firstName, lastName } = req.body;
 
-  if (typeof username !== 'string') {
-    return res.status(422).json({message: 'Incorrect field type: username'});
+  if (typeof username !== "string") {
+    return res.status(422).json({ message: "Incorrect field type: username" });
   }
 
   username = username.trim();
 
-  if (username === '') {
-    return res.status(422).json({message: 'Incorrect field length: username'});
+  if (username === "") {
+    return res
+      .status(422)
+      .json({ message: "Incorrect field length: username" });
   }
 
-  if (!(password)) {
-    return res.status(422).json({message: 'Missing field: password'});
+  if (!password) {
+    return res.status(422).json({ message: "Missing field: password" });
   }
 
-  if (typeof password !== 'string') {
-    return res.status(422).json({message: 'Incorrect field type: password'});
+  if (typeof password !== "string") {
+    return res.status(422).json({ message: "Incorrect field type: password" });
   }
 
   password = password.trim();
 
-  if (password === '') {
-    return res.status(422).json({message: 'Incorrect field length: password'});
+  if (password === "") {
+    return res
+      .status(422)
+      .json({ message: "Incorrect field length: password" });
   }
-  return User
-    .find({username})
+  return User.find({ username })
     .exec()
-    .then((users) => {
+    .then(users => {
       return users[0].validatePassword(password).then(isValid => {
-        if(isValid) {
+        if (isValid) {
           return res.status(200).json(users[0].apiRepr());
+        } else {
+          return res.status(401).json({ message: "Invalid password!" });
         }
-        else{
-          return res.status(401).json({message:"Invalid password!"});
-        }
-      })
+      });
     })
-    .catch(err => console.log(err) && res.status(500).json({message: 'Internal server error'}));
+    .catch(
+      err =>
+        console.log(err) &&
+        res.status(500).json({ message: "Internal server error" })
+    );
 });
 
 const basicStrategy = new BasicStrategy(function(username, password, callback) {
   let user;
-  User
-    .findOne({username: username})
+  User.findOne({ username: username })
     .exec()
     .then(_user => {
       user = _user;
       if (!user) {
-        return callback(null, false, {message: 'Incorrect username'});
+        return callback(null, false, { message: "Incorrect username" });
       }
       return user.validatePassword(password);
     })
     .then(isValid => {
       if (!isValid) {
-        return callback(null, false, {message: 'Incorrect password'});
-      }
-      else {
-        return callback(null, user)
+        return callback(null, false, { message: "Incorrect password" });
+      } else {
+        return callback(null, user);
       }
     });
 });
 
-
 passport.use(basicStrategy);
 router.use(passport.initialize());
 
-router.get('/me',
-  passport.authenticate('basic', {session: false}),
-  (req, res) => res.json({user: req.user.apiRepr()})
+router.get(
+  "/me",
+  passport.authenticate("basic", { session: false }),
+  (req, res) => res.json({ user: req.user.apiRepr() })
 );
 
-
-module.exports = {router};
+module.exports = { router };
